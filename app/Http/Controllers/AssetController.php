7fb2d6acd778ab7bc5a4b74974113ae58c7096d2
waseem ali <aliw2025@gmail.com>
2023-04-26 12:53:53 +0500

    done maunal file addition. next step is to add pictures

--- app/Http/Controllers/AssetController.php
+++ app/Http/Controllers/AssetController.php
@@ -58,6 +58,9 @@ class AssetController extends Controller
      */
     public function store(Request $request)
     {
+        // $request->validate([
+        //     'file_name' => 'required'
+        // ]);
         //
         $asset = new Asset();
         $asset->asset_technical_category = $request->asset_tech_cat;
@@ -85,6 +88,17 @@ class AssetController extends Controller
         $asset->building_block = $request->building_block;
         $asset->floor= $request->floor;
         $asset->department=$request->deparment;
+
+        if($request->hasFile('file_name')) {
+            $file = $request->file('file_name');
+            // dd($file);s
+            $fileName = $file->getClientOriginalName();   
+            $filePath = $request->file('file_name')->storeAs('uploads', $fileName, 'public');
+            $asset->manual_file_name = $file->getClientOriginalName();
+            $asset->manual_file_path = url('/').'/public/storage/' . $filePath;
+        
+        }
+        
         $asset->save();
 
         $vendors = Vendor::all();
@@ -118,6 +132,9 @@ class AssetController extends Controller
     public function show(Asset $asset)
     {
         //
+
+        return redirect()->route('select-asset',$asset->id);
+        
     }
 
     /**
@@ -141,6 +158,54 @@ class AssetController extends Controller
     public function update(Request $request, Asset $asset)
     {
         //
+        $asset->asset_technical_category = $request->asset_tech_cat;
+        $asset->equipment_category_name = $request->equipment_category_name;
+        $asset->equipment_type = $request->equipment_type;
+        $asset->manufacturer=$request->manufacturer;
+        $asset->model = $request->model;
+        $asset->serial_number=$request->serial_number;
+        $asset->fa_number = $request->fa_number;
+        $asset->equipment_sequence_number = $request->equipment_seq_number;
+        $asset->manufacture_date = $request->manufacture_date;
+        $asset->installation_date = $request->installation_date;
+        $asset->equipment_status = $request->asset_status;
+        // $asset->manual_file_path;
+        $asset->vendor = $request->vendor;
+        $asset->room_area = $request->room_area;
+        $asset->section=$request->section;
+        $asset->sub_section= $request->sub_section;
+        $asset->custodian_name=$request->custodian_name;
+        $asset->office_extention=$request->custodian_ofc_ext;
+        $asset->mobile=$request->custodian_mobile;
+        $asset->email=$request->custodian_email;
+        // $asset->last_ppm_date;
+        // $asset->next_ppm_date;
+        $asset->building_block = $request->building_block;
+        $asset->floor= $request->floor;
+        $asset->department=$request->deparment;
+
+        if($request->hasFile('file_name')) {
+            
+            $file = $request->file('file_name');
+            // dd($file);s
+            $fileName = $file->getClientOriginalName();   
+            $filePath = $request->file('file_name')->storeAs('uploads', $fileName, 'public');
+            $asset->manual_file_name = $file->getClientOriginalName();
+            $asset->manual_file_path = url('/').'/public/storage/' . $filePath;
+        
+        }
+        
+        $asset->save();
+
+        $vendors = Vendor::all();
+        $floors = TblFloor::all();
+        $eq_status = TblEquipmentStatus::all();
+        $departments = TblDepartment::all();
+        $blocks = TblBuildingBlock::all();
+        // $asset->ppm_type;
+        return view('Assets.add-asset',compact('asset','eq_status','floors','departments','vendors','blocks'));
+
+
     }
 
     /**
