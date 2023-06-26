7ec6261de9f86f5203eb1ef0cda74f47adf32ca5
waseem ali <aliw2025@gmail.com>
2023-06-26 13:58:48 +0500

    added

--- resources/views/work-order/resolution.blade.php
+++ resources/views/work-order/resolution.blade.php
@@ -2,14 +2,29 @@
 
     <div class="col-12">
         <div id="accordion4" class="row">
-            <div>
+            <div class="col-12">
+                @php
+                $count = 1
+                @endphp
                 @foreach($workOrder->resolutions as $res)
-                
-                   
 
-                    <textarea disabled name="resolution" class="summernotes"> {{$res->resolution}}</textarea>
-
-
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Resolution # {{$count}}</h4>
+                    </div>
+                    <div class="card-body">
+                        @php
+                           $text = strip_tags ($res->resolution);
+                           $text = preg_replace('/\s*style="[^"]+"/i', '', $text);
+                           $text = str_replace('&nbsp;', '', $text);
+                        @endphp
+                        {{ ($text) }}
+                    </div>
+                </div>
+                <!-- <textarea disabled name="resolution" class="summernotes"> </textarea> -->
+                @php
+                $count= $count+1
+                @endphp
                 @endforeach
             </div>
             <div class="col-12 d-flex justify-content-end">
@@ -22,8 +37,8 @@
                     <form action="{{route('add-resolution')}}" method="POST">
                         @csrf
                         <input type="hidden" name="work_order_id" value="{{$workOrder->id}}">
-                        <label for="">Resolution</label>
-                        <textarea name="resolution" id="summernote2">{{$workOrder->resolution}}</textarea>
+                        <label for="">Resolutionsdsdss</label>
+                        <textarea name="resolution" id="summernote2">{{strip_tags($workOrder->resolution)}}</textarea>
                         <button action="sumbit" class="btn btn-primary">Add</button>
                     </form>
 
